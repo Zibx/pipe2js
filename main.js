851bbf29cbc0fecec2d42752aae7c884595ba83f
Ivan Kubota <kubota@form.dev>
2023-04-24 12:34:46 +0400

    File tests. Add dependency for stram JSON parsing. Now we support filetring huge logs made of JSON objects. Fix some bugs with stop working.

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
 #!/usr/bin/env node
 
-var fnFactory = function(processFn, nextFn, finishFn){
-	return function(){
-		currentProcessor = new Processor();
+var StreamValues = require('stream-json/streamers/StreamValues');
+var stream = require('stream');
+var debug = function nop() {};
+var fnFactory = function( processFn, nextFn, finishFn ) {
+	return function(awk, args) {
+		var currentProcessor = new Processor(null, awk);
 		currentProcessor.fn = this.step;
-		pipeline.push(currentProcessor);
+		currentProcessor.name = this.name;
+		currentProcessor.usedArgs = [];
+		awk.pipeline.push( currentProcessor );
 
-		while(true){
-			var arg = getArg();
+		while( true ) {
+			var arg = args.getArg();
 
-			if(arg === void 0)
+			if( arg === void 0 )
 				arg = 'a';
 
-			if(arg in keys) {
-				if(keys[arg].primary) {
-					returnArg();
+			if( arg in keys ) {
+				if( keys[ arg ].primary ) {
+					args.returnArg();
 					arg = 'a';
 				}
 			}
+			currentProcessor.usedArgs.push(arg);
 
-			if(arg in keys){
-				keys[arg].fn(currentProcessor);
-			}else{
-				if(processFn) {
-					currentProcessor.fn = processFn.call( currentProcessor, arg );
+			if( arg in keys ) {
+				keys[ arg ].fn( currentProcessor );
+			} else {
+				if( processFn ) {
+					try {
+						currentProcessor.fn = processFn.call( currentProcessor, arg );
+					}catch( e ){
+						console.error('Error parsing arguments. Syntax error. Verbose dump')
+						args.parsed.forEach(function(parsed, n){
+							console.error('\t'+ (n+1) + ') '+ parsed.join(' '))
+						})
+						console.error('\n\t'+ (args.parsed.length+1) + ') '+ this.name + ' '+ currentProcessor.usedArgs.join(' '))
+						//var [, lineno, colno] = e.stack.match(/<anonymous>:(\d+):(\d+)/);
+						console.error(e.message);
+						process.exit(1);
+						throw new Error(e.message);
+					}
 				}
 				currentProcessor.next = nextFn;
 				currentProcessor.finishFn = finishFn;
@@ -33,14 +51,26 @@ var fnFactory = function(processFn, nextFn, finishFn){
 	}
 };
 
-var callNext = function(pipelineCursor){
-	if(pipelineCursor < pipeline.length) {
-		pipeline[ pipelineCursor + 1 ].consume( this.current, pipelineCursor+1 );
+var callFinish = function(pipelineCursor){
+  if( pipelineCursor < this.awk.pipeline.length - 1 ) {
+    this.awk.pipeline[ pipelineCursor + 1 ].finish( pipelineCursor + 1 );
+  }else{
+    this.awk.finished = true;
+    this.awk.afterEnd && this.awk.afterEnd();
+  }
+}
+var callNextAndFinish = function(pipelineCursor){
+  callNext.call(this, pipelineCursor);
+  callFinish.call(this, pipelineCursor);
+};
+var callNext = function( pipelineCursor ) {
+	if( pipelineCursor < this.awk.pipeline.length ) {
+		this.awk.pipeline[ pipelineCursor + 1 ].consume( this.current, pipelineCursor + 1 );
 	}
 };
 
-var codeDecorator = function(fn){
-	return function(currentProcessor) {
+var codeDecorator = function( fn ) {
+	return function( currentProcessor ) {
 		currentProcessor.decorators.push( fn );
 	}
 };
@@ -57,17 +87,17 @@ var keys = {
 		alias: '-flat',
 		primary: true,
 		description: 'convert array to separate items',
-		fn: fnFactory(function(code){
+		fn: fnFactory( function( code ) {
 			return bypass;
-		}, function(pipelineCursor){
-			if(pipelineCursor < pipeline.length) {
-				if('length' in this.current){
-					for(var i = 0, _i = this.current.length; i < _i; i++){
-						pipeline[ pipelineCursor + 1 ].consume( this.current[i], pipelineCursor+1 );
+		}, function( pipelineCursor ) {
+			if( pipelineCursor < this.awk.pipeline.length ) {
+				if( 'length' in this.current ) {
+					for( var i = 0, _i = this.current.length; i < _i; i++ ) {
+						this.awk.pipeline[ pipelineCursor + 1 ].consume( this.current[ i ], pipelineCursor + 1 );
 					}
 				}
 			}
-		})
+		} )
 
 	},
 	'--json': {
@@ -81,36 +111,73 @@ cat package.json | pipe2js -j -m 'obj.version'
 2. We have got an array of objects, list only ids of objects that have amount=10. Objects looks like: { id: '10p-n-1wu', amount: 5, win: true }
 cat tickets.json | pipe2js -j --flat -f "obj.amount===10" -m -t '$\{obj.win?"+":"-"}$\{obj.id}'
 `,
-		fn: fnFactory(function(code){
+		fn: fnFactory( function( code ) {
+      this.jsonParser = StreamValues.withParser();
+      var _self = this;
+
+      this.jsonParser.on('end', (data) => {
+        callFinish.call(_self, _self.pipelineCursor);
+        _self.jsonParser.end();
+        //_self.awk.pipeline[ _self.pipelineCursor + 1 ].finish( _self.pipelineCursor + 1 );
+      });
+      this.jsonParser.on('data', (data) => {
+        var obj = data.value;
+        _self.awk.pipeline[ _self.pipelineCursor + 1 ].consume( obj, _self.pipelineCursor + 1 );
+      });
+      this.readable = new stream.Readable();
+      this.readable._read = function(){
+          
+      }
+      //new ReadString('{"a":1}{"b":2}').pipe(this.jsonParser);
+      //this.jsonParser = new parser({jsonStreaming: true, packValues: true, streamValues: false})
 			this.exp = code;
-			return new Function('', 'return (a, b, i, total, line, obj)=>i>0?b+"\\n"+a:a')();
-		}, function(pipelineCursor) {
+      debug('init pipe to parser');
+      this.readable.pipe(this.jsonParser);
+      debug('after init pipe to parser');
 
-		}, function(pipelineCursor){
-			try {
+      return new Function( '', 'return (a, b, i, total, line, obj)=>a' )();
+			//return new Function( '', 'return (a, b, i, total, line, obj)=>i>0?b+"\\n"+a:a' )();
+		}, function( pipelineCursor ) {
+      debug('before write to readable')
+      this.readable.push(this.current, 'utf8');
+      //new ReadString(this.current).pipe(this.jsonParser);
+      this.pipelineCursor = pipelineCursor;
+		}, function( pipelineCursor ) {
+
+			/*try {
 				var obj = JSON.parse( this.current );
-			}catch( e ){
-				console.error(e);
-				process.exit(1);
-			}
-
-			pipeline[ pipelineCursor + 1 ].consume( obj, pipelineCursor+1 );
-		})
+			} catch( e ) {
+				console.error( e );
+				process.exit( 1 );
+			}*/
+      debug('finish write to readable')
+      this.readable.push(null);
+      //new ReadString('').pipe(this.jsonParser);
+			//this.awk.pipeline[ pipelineCursor + 1 ].consume( obj, pipelineCursor + 1 );
+			//this.awk.pipeline[ pipelineCursor + 1 ].finish( pipelineCursor + 1 );
+		} )
 	},
 	'--contain': {
 		description: 'check if line contains string',
 		alias: '-c',
-		fn: codeDecorator(function(code){
-		    return 'line.indexOf( `'+code+'` )>-1'
-		})
+		fn: codeDecorator( function( code ) {
+			return 'line.indexOf( `' + code + '` )>-1'
+		} )
 	},
 	'--regexp': {
 		description: 'use regexp to filter',
 		alias: '-x',
-		fn: codeDecorator(function(code){
-			return 'line.match( /'+code+'/ )!==null'
-		})
+		fn: codeDecorator( function( code ) {
+			return 'line.match( /' + code + '/ )!==null'
+		} )
 	},
+  '--keys': {
+    description: 'assign array elements to named keys',
+    alias: '-k',
+    fn: codeDecorator( function( code ) {
+      return 'Array.isArray(line) ? `'+code+'`.split(",").map(x=>x.trim()).map((key,n)=>({key, val: line[n]})).reduce((store, kv)=>{store[kv.key] = kv.val; return store}, {}) : line'
+    } )
+  },
 	'--map': {
 		primary: true,
 		alias: '-m',
@@ -131,9 +198,9 @@ Usage Example:
 
 2. Generating some sequence (0 1 3 6 10 15 ...) 
 > ping 8.8.8.8 | pipe2js -m 'i+previous|0'`,
-		fn: fnFactory(function(code){
-			return new Function('', 'return (line, previous, i, total, a, obj)=>(\n'+this.decorate(code)+'\n)')();
-		}, callNext)
+		fn: fnFactory( function( code ) {
+			return new Function( '', 'return (line, previous, i, total, a, obj)=>(\n' + this.decorate( code ) + '\n)' )();
+		}, callNext )
 	},
 	'--reduce': {
 		primary: true,
@@ -141,7 +208,7 @@ Usage Example:
 		moreDescription: `
 Avaliable Variables: 
   \`a\` - current item
-  \`b\` - current item
+  \`b\` - storage / previous item / previous reduce result
   \`i\`         - line number
   \'total\`     - total number of consumed lines
   
@@ -153,17 +220,17 @@ Usage Example:
 2. Generating some sequence (0 1 3 6 10 15 ...) 
 > ping 8.8.8.8 | pipe2js -m 'i+previous|0'`,
 		alias: '-r',
-		fn: fnFactory(function(code){
-			return new Function('', 'return (a, b, i, total)=>i>0?(\n'+this.decorate(code)+'\n):a')();
-		}, function(pipelineCursor) {
+		fn: fnFactory( function( code ) {
+			return new Function( '', 'return (a, b, i, total)=>i>0?(\n' + this.decorate( code ) + '\n):a' )();
+		}, function( pipelineCursor ) {
 
-		}, callNext)
+		}, callNextAndFinish )
 	},
 	'--number': {
 		alias: '-n',
 		description: 'convert input to number. Operates with string may cause concat instead of sum',
-		fn: function(processor){
-			processor.preprocess.push(parseFloat);
+		fn: function( processor ) {
+			processor.preprocess.push( parseFloat );
 		}
 	},
 	'--tpl': {
@@ -171,9 +238,9 @@ Usage Example:
 		description: 'code is a template',
 		moreDescription: `
 ping 8.8.8.8 | pipe2js -m 'line.split("time=")[1]' -f -n -r "a+b" -m -t 'sum: $\{a}, total: $\{total}, avg: $\{a/total}'`,
-		fn: codeDecorator(function(code){
-			return '`'+code+'`'
-		})
+		fn: codeDecorator( function( code ) {
+			return '`' + code + '`'
+		} )
 	},
 	'--filter': {
 		primary: true,
@@ -189,42 +256,42 @@ Avaliable Variables:
 Usage Example:
   ping 8.8.8.8 | pipe2js -f 'parseInt(line.split("icmp_seq=")[1])>2'
   ping 8.8.8.8 | pipe2js -f -c '3'`,
-		fn: fnFactory(function(code){
-			return new Function('', 'return (line, previous, i, total, a, obj)=>(\n'+this.decorate(code)+'\n)')();
-		}, function(pipelineCursor){
-			if(this.current && pipelineCursor < pipeline.length) {
-				pipeline[ pipelineCursor + 1 ].consume( this.processed, pipelineCursor+1 );
+		fn: fnFactory( function( code ) {
+			return new Function( '', 'return (line, previous, i, total, a, obj)=>(\n' + this.decorate( code ) + '\n)' )();
+		}, function( pipelineCursor ) {
+			if( this.current && pipelineCursor < this.awk.pipeline.length ) {
+				this.awk.pipeline[ pipelineCursor + 1 ].consume( this.processed, pipelineCursor + 1 );
 			}
-		})
+		} )
 	},
 	'--version': {
 		alias: '-v',
 		description: 'display version of and exit',
 		fn: function() {
-			console.log('v'+JSON.parse(require('fs').readFileSync('package.json')).version);
-			process.exit(0);
+			console.log( 'v' + JSON.parse( require( 'fs' ).readFileSync( 'package.json' ) ).version );
+			process.exit( 0 );
 		}
 	},
 	'--help': {
 		description: 'display this help and exit',
 		alias: '-h',
-		fn: function(){
-		    console.log(
-`Usage: cat [FILE] | pipe2js [OPTION]...
+		fn: function() {
+			console.log(
+				`Usage: cat [FILE] | pipe2js [OPTION]...
 Use javaScript flavor to Map, Filter, Reduce standard input to standard output. This tool use stream and can process huge files.
 
 Options:
-${helpKeys.map(key => {
-	var pad = function(count, symbol){
-	    return count > 0 ? new Array(count+1).join(symbol) : ''; 
+${helpKeys.map( key => {
+	var pad = function( count, symbol ) {
+		return count > 0 ? new Array( count + 1 ).join( symbol ) : '';
 	};
-	var param = keys[key];
-	var prop = `${param.alias?param.alias+',': '   '} ${key}`;
-	
-	return `  ${prop}${pad(16-prop.length, ' ')}${param.description}${
-		param.moreDescription? '\n'+param.moreDescription.split('\n').map(a=>pad(18,' ')+a).join('\n')+'\n': '' 
+	var param = keys[ key ];
+	var prop = `${param.alias ? param.alias + ',' : '   '} ${key}`;
+
+	return `  ${prop}${pad( 16 - prop.length, ' ' )}${param.description}${
+		param.moreDescription ? '\n' + param.moreDescription.split( '\n' ).map( a => pad( 18, ' ' ) + a ).join( '\n' ) + '\n' : ''
 	}`
-}).join('\n')}
+} ).join( '\n' )}
 
 You can use any count of maps, filters. and reduces
 
@@ -239,144 +306,220 @@ Examples:
     
   cat log.log | pipe2js -f -c 'win ' -m 'a.split("TYPE:")[1]' -m -n -r 'a+b'
   
-`);
-			process.exit(0);
+` );
+			process.exit( 0 );
 		}
 	}
 };
-var helpKeys = Object.keys(keys).sort();
+var helpKeys = Object.keys( keys ).sort();
 
 // aliases
-helpKeys.forEach(function(key){
-	var param = keys[key];
+helpKeys.forEach( function( key ) {
+	var param = keys[ key ];
 	param.name = key;
-	if(param.alias){
-		keys[param.alias] = param;
+	if( param.alias ) {
+		keys[ param.alias ] = param;
 	}
-});
-
-var currentArgPointer = 2,
-	argCount = process.argv.length;
-
-var returnArg = function(){
-	currentArgPointer--;
-}
-var getArg = function(){
-    return process.argv[currentArgPointer++];
-};
+  if( param.aliases ) {
+    param.aliases.forEach(function(alias){
+      keys[ alias ] = param;
+    })
+  }
+} );
 
 var bypass = a => a;
-var total = 0;
 
-var Processor = function(consume){
-    this.preprocess = [];
+var Processor = function( consume, awk ) {
+	this.preprocess = [];
 	this.postprocess = [];
 	this.decorators = [];
 	this.cursor = 0;
 	this.fn = bypass;
-	consume && (this.consume = consume);
+	this.awk = awk;
+	consume && ( this.consume = consume );
 };
 Processor.prototype = {
+	state: 'ACTIVE',
 	fn: bypass,
-	consume: function(line, pipelineCursor){
-	  var processed = this.preprocess.reduce(function(val, fn){
-	      return fn(val);
-	  }, line);
-	  this.previous = this.current;
-	  this.processed = processed;
-	  this.current = this.fn(processed, this.previous, this.cursor, total, processed, processed);
+	consume: function( line, pipelineCursor ) {
+		var processed = this.preprocess.reduce( function( val, fn ) {
+			return fn( val );
+		}, line );
+		this.previous = this.current;
+		this.processed = processed;
+		try {
+			this.current = this.fn( processed, this.previous, this.cursor, this.awk.total, processed, processed );
+		}catch( e ){
+			console.error('Error in runtime. Syntax error. Verbose dump')
+			this.awk.args.parsed.forEach(function(parsed, n){
+				console.error('\t'+ (n+1) + ') '+ parsed.join(' '))
+			});
+			var [, lineno, colno] = e.stack.match(/<anonymous>:(\d+):(\d+)/);
 
-	  this.current = this.postprocess.reduce(function(val, fn){
-			return fn(val);
-	  }, this.current);
-
-	  this.next(pipelineCursor);
-      this.cursor++;
-	},
-	finish: function(pipelineCursor){
-	    if(this.finishFn)
-			this.finishFn(pipelineCursor);
-		else{
-			if( pipelineCursor < pipeline.length -  1 ) {
-				pipeline[ pipelineCursor + 1 ].finish( pipelineCursor + 1 );
+			var pad = function(num){
+			    return num > 0 ? new Array(num+1).join(' '): '';
 			}
+			console.error('\n\t'+ (this.awk.args.parsed.length+1) + ') '+ this.name + ' '+ this.usedArgs.join(' '))
+			console.error('\t'+ pad(((this.awk.args.parsed.length+1) + ') '+ this.name + ' ').length+(colno-0)-1) +'^ '+ e.message)
+
+			//process.exit(1);
+			throw new Error(e.message);
 		}
+		this.current = this.postprocess.reduce( function( val, fn ) {
+			return fn( val );
+		}, this.current );
+
+		this.next( pipelineCursor );
+		this.cursor++;
 	},
-	decorate: function(code){
-	    return this.decorators.reduce(function(code, fn){
-	        return fn(code);
-	    }, code)
+	finish: function( pipelineCursor ) {
+		this.state = 'FINISHING';
+		if( this.finishFn ) {
+      this.finishFn( pipelineCursor );
+
+    } else {
+			if( pipelineCursor < this.awk.pipeline.length - 1 ) {
+				this.awk.pipeline[ pipelineCursor + 1 ].finish( pipelineCursor + 1 );
+			}else{
+        this.awk.finished = true;
+        this.awk.afterEnd && this.awk.afterEnd();
+      }
+		}
+		this.state = 'FINISHED';
+	},
+	decorate: function( code ) {
+		return this.decorators.reduce( function( code, fn ) {
+			return fn( code );
+		}, code )
+	}
+};
+
+var AWK = function() {
+	this.total = 0;
+};
+AWK.prototype = {
+	parseArgs: function(args){
+
+		this.args = {
+			raw: args,
+			argCount: args.length,
+			currentArgPointer: 0,
+			getArg: function() {
+				return args[ this.currentArgPointer++ ];
+			},
+			returnArg: function() {
+				this.currentArgPointer--;
+			},
+			parsed: []
+		};
+		this.pipeline = [];
+
+		while( this.args.currentArgPointer < this.args.argCount ){
+			var argStart = this.args.currentArgPointer;
+			var arg = this.args.getArg();
+			if(arg in keys){
+				keys[arg].fn(this, this.args);
+			}else{
+				console.error(`Unknown parameter ${arg}`);
+				process.exit(1);
+			}
+			var argEnd = this.args.currentArgPointer;
+			this.args.parsed.push(args.slice(argStart, argEnd))
+		}
+
+		var _self = this;
+    var outputProcessor = new Processor(function(line){
+      _self.output(line);
+    }, this);
+    outputProcessor.finishFn = function(){
+        this.awk.finished = true;
+        this.awk.afterEnd && this.awk.afterEnd();
+    };
+		this.pipeline.push(outputProcessor);
+
+		this.pipeline[this.pipeline.length - 1].name = 'output'
+
+	},
+	output: function(line){
+		console.log(line);
+		/*process.stdout.write(line);*/
+	},
+	consume: function(data){
+		this.total++;
+		return this.pipeline[0].consume(data, 0);
+	},
+	finish: function(){
+		this.pipeline[ 0 ].finish( 0 );
 	}
 };
 
 
-var currentProcessor,
-	pipeline = [];
-
-while( currentArgPointer < argCount ){
-	var arg = getArg();
-	if(arg in keys){
-		keys[arg].fn();
-	}else{
-		console.error(`Unknown parameter ${arg}`);
-		process.exit(1);
-	}
-}
 
 
-pipeline.push(new Processor(function(line){
-	console.log(line);
-		/*process.stdout.write(line);*/
-}))
 
 /*var code = process.argv.slice(2).join(' ');
 console.log(process.argv)*/
 //var fn = new Function('', 'return (line, i)=>(\n'+code+'\n)')();
 
-const DEBUG = false;
-if(DEBUG) {
-	var fs = require( 'fs' );
-	var data = fs.readFileSync( 'test/filterData.log' ).toString().split( /\r\n|\n/ );
-	/*var data = fs.readFileSync('test/testData.log').toString().split(/\r\n|\n/);*/
-	data.forEach( function( line ) {
-		pipeline[ 0 ].consume( line, 0 );
-	} );
-	pipeline[ 0 ].finish( 0 );
 
-	process.exit( 0 )
+
+
+if (typeof require !== 'undefined' && typeof module !== 'undefined' && require.main === module) {
+	var awk = new AWK();
+	awk.parseArgs([].slice.call(process.argv, 2));
+
+	const DEBUG = false;
+  var fs = require( 'fs' );
+
+  if(DEBUG) {
+		var fs = require( 'fs' );
+		var data = fs.readFileSync( 'test/data/filterData.log' ).toString().split( /\r\n|\n/ );
+		/*var data = fs.readFileSync('test/testData.log').toString().split(/\r\n|\n/);*/
+		data.forEach( function( line ) {
+			awk.consume( line );
+		} );
+		awk.finish( );
+
+		process.exit( 0 )
+	}
+
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: false
+	});
+
+	var currentLineNumber = 0;
+
+	rl.on('line', (line) => {
+		line = line.toString();
+
+/*    fs.appendFile("verbose.txt", line, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+
+      }
+    });*/
+		awk.consume(line, 0);
+	});
+
+	rl.once('close', () => {
+		// end of input
+		awk.finish();
+	});
+
+	process.on('SIGINT', function() {
+		awk.finish();
+    awk.afterEnd = function() {
+      process.exit();
+    }
+	});
+}else{
+	module.exports = AWK;
 }
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
-var currentLineNumber = 0;
-
-rl.on('line', (line) => {
-	line = line.toString();
-	total++;
-	pipeline[0].consume(line, 0);
-	//process.stdout.write(fn(line, currentLineNumber)+'\n');
-	//currentLineNumber++;
-});
-
-rl.once('close', () => {
-	pipeline[ 0 ].finish( 0 );
-
-	// end of input
-});
-
-process.on('SIGINT', function() {
-	pipeline[ 0 ].finish( 0 );
-
-	//setTimeout(function(){
-		process.exit();
-	//}, 10);
-
-});
-
 /*process.stdin.on( 'end', function() { console.log( 'EOF' ) } );*/
 
 /*process.stdin.on("data", data => {
---
+++ test/fileTests.js
@@ -0,0 +1,127 @@
+var fs = require('fs'),
+	path = require('path');
+
+var assert = require('assert');
+var parser = require('shell-quote').parse;
+var sinon = require("sinon");
+
+var lists = {};
+
+var AWK = require('../');
+
+['request', 'input', 'output'].forEach(function(name){
+	lists[name] = fs.readdirSync(path.join(__dirname, name));
+});
+
+//lists.request = ['fileWithJSONs'];
+describe('Test all cases', function() {
+	lists.request.forEach( function( fileName ) {
+		it( `Testing ${fileName}`, function(done) {
+			var argsData = fs.readFileSync(path.join(__dirname, 'request', fileName)).toString('utf-8');
+			var argsLines = argsData.split(/\r\n|\n/);
+			var argsComments = argsLines
+				.filter(line => line[0] === '#')
+				.map(line => line.substr(1).trim());
+			if(argsComments)
+				console.log(argsComments.join('\n'));
+
+			var args = parser(argsLines.filter(a=>a[0] !== '#').join('\n'));
+
+			console.log(`parsed arguments: ${JSON.stringify(args)}`)
+
+			var stub = sinon.stub(process, 'exit');
+			var awk = new AWK();
+      try {
+        awk.parseArgs( args );
+      }catch(e){
+        stub.restore();
+        if(argsComments.filter(line => line === 'EXCEPTION').length){
+          var exceptionText = argsComments.filter(line => line.indexOf('EXCEPTION TEXT:')>-1);
+          if(exceptionText.length){
+            var text = exceptionText.join('\n').split('EXCEPTION TEXT:')[1].trim();
+            if(e.message.indexOf(text)>-1){
+              console.log('Exception was expected');
+            }else{
+              throw new Error(`Error message does not match: ${e.message} vs ${text}`);
+            }
+          }else {
+            console.log( 'Exception was expected' )
+          }
+          return done();
+        }else{
+          throw new Error();
+        }
+        return done();
+      }
+			stub.restore();
+
+			if(stub.calledOnce){
+				if(argsComments.filter(line => line === 'EXCEPTION')){
+					console.log('Exception was expected')
+					return done();
+				}else{
+					throw new Error();
+				}
+				return done();
+			}
+
+			var inputData = fs.readFileSync(path.join(__dirname, 'input', fileName)).toString('utf-8');
+			var lines = inputData.split(/\r\n|\n/);
+
+			var result = [];
+			awk.output = function(data){
+			    result.push(data);
+			};
+      awk.afterEnd = function(){
+        var outputData = fs.readFileSync(path.join(__dirname, 'output', fileName)).toString('utf-8');
+        var [is, must] = [result.join('\n').trim(), outputData.split(/\r\n|\n/).join('\n').trim()];
+        if(is !== must){
+          console.log('Does not match, verbose output');
+          awk.pipeline.forEach((pipeline, i) =>{
+            console.log(`STEP ${i+1} ${pipeline.name}`);
+            console.log({processed: pipeline.processed, current: pipeline.current})
+            console.log('\n');
+          });
+        }else{
+          console.log('Result matched: ', result.join('\n').trim())
+        }
+        assert.equal(is, must);
+        done();
+      };
+
+			try {
+				var stub = sinon.stub( process, 'exit' ).callsFake( () => {
+					throw new Error( 'Error' );
+				} );
+				for( var i = 0; i < lines.length; i++ ) {
+					var line = lines[ i ];
+					awk.consume( line );
+				}
+				awk.finish();
+				stub.restore();
+			}catch(e){
+				stub.restore();
+				if(argsComments.filter(line => line === 'EXCEPTION').length){
+					var exceptionText = argsComments.filter(line => line.indexOf('EXCEPTION TEXT:')>-1);
+					if(exceptionText.length){
+						var text = exceptionText.join('\n').split('EXCEPTION TEXT:')[1].trim();
+						if(e.message.indexOf(text)>-1){
+							console.log('Exception was expected');
+						}else{
+							throw new Error(`Error message does not match: ${e.message} vs ${text}`);
+						}
+					}else {
+						console.log( 'Exception was expected' )
+					}
+					return done();
+				}else{
+					throw new Error();
+				}
+				return done();
+
+			}
+
+
+		} );
+	} );
+});
